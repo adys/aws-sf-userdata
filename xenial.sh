6d36ad607af2a5ac53a012681dae74095176c540
Andrej Krnac <andrej.krnac@piano.io>
2019-08-16 16:10:35 +0200

    Add default gateway

--- xenial.sh
+++ xenial.sh
@@ -105,14 +105,15 @@ netmask ${SUBNET_FULL_MASK}
 
 # Gateway configuration
 up ip route add default via ${SUBNET_GW} dev ${STATIC_INTERFACE_NAME} table 1000
+up ip route add default via 172.20.32.1
 
 # Routes and rules
 up ip route add ${STATIC_IP} dev ${STATIC_INTERFACE_NAME} table 1000
 up ip rule add from ${STATIC_IP} lookup 1000
 EOF
   log_info "'${NET_CONF_FILE_PATH}' file generated"
-  #log_info "Running 'systemctl restart networking'"
-  #systemctl restart networking
+  log_info "Running 'systemctl restart networking'"
+  systemctl restart networking
 }
 
 setup_data_dir() {
